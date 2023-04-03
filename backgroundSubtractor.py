247f74faa53a96611cc1c43ea3adaa6d98fc067d
BYUNG <45321943+ilovebyung@users.noreply.github.com>
2023-04-03 21:07:31 +0900

    createBackgroundSubtractorKNN

--- backgroundSubtractor.py
+++ backgroundSubtractor.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # combine background & foreground images into out.mp4
 # ffmpeg -pattern_type glob -i '*.jpeg' out.mp4
-os.chdir('d:/source/lighting/objects/bg')
+os.chdir('./objects/bg')
 cap = cv2.VideoCapture("out.mp4")
 # pBackSub = cv2.createBackgroundSubtractorMOG2()
 pBackSub = cv2.createBackgroundSubtractorKNN()
@@ -49,34 +49,36 @@ ret, threshold = cv2.threshold(
 plt.imshow(threshold, cmap='gray')
 
 '''
-combined threshold
-'''
-# 1.Erosion
-kernel = np.ones((20, 20), np.uint8)
-erosion = cv2.erode(fgMask, kernel, iterations=1)
-# 2.Closing
-closing = cv2.morphologyEx(erosion, cv2.MORPH_CLOSE, kernel)
-plt.imshow(closing, cmap='gray')
-# 3.Dilation
-dilation = cv2.dilate(closing, kernel, iterations=1)
-plt.imshow(dilation, cmap='gray')
-# 4.Otsu's thresholding
-ret, threshold = cv2.threshold(
-    dilation, 100, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-plt.imshow(threshold, cmap='gray')
-
-'''
 background, foreground
 '''
 # Load image, create mask, and draw white circle on mask
 mask = threshold
-image = cv2.imread('d:/source/lighting/objects/bg/99.jpeg', 0)
+image = cv2.imread('99.jpeg', 0)
 
 # Mask input image with binary mask
 result = cv2.bitwise_and(image, mask)
 # Color background white
 result[mask < 100] = 255  # Optional
+result[mask >= 100] = 0  # Optional
 plt.imshow(result, cmap='gray')
+
+'''
+combined threshold
+'''
+# 2.Closing
+closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+plt.imshow(closing, cmap='gray')
+# 3.Dilation
+dilation = cv2.dilate(closing, kernel, iterations=1)
+plt.imshow(dilation, cmap='gray')
+
+# Mask input image with binary mask
+result = cv2.bitwise_and(image, closing)
+plt.imshow(result, cmap='gray')
+
+cv2.imshow('result', result)
+cv2.waitKey(0)
+cv2.destroyWindow()
 
 '''
 contour approximation
